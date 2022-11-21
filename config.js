25af63ead4e6ccf06d6fd6a512895bfe60dd3d17
Amir Fakouri <98204356+Amir-fa@users.noreply.github.com>
2022-11-21 12:02:47 +0330

    instagram stories

---
+++ config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const color = require('tailwindcss/colors');
+module.exports = {
+  theme: {
+    extend: {
+      color: {
+        fuchsia: colors.fuchsia,
+      },
+    },
+  },
+  plugins: [],
+};
